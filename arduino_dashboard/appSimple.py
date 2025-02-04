02e2b32ba74aff7a34136229fbf8c1e853615d30
zoelzw <ziwei_liu3@apple.com>
2025-02-04 01:31:42 -0800

    dummy serial update working

--- arduino_dashboard/appSimple.py
+++ arduino_dashboard/appSimple.py
@@ -8,13 +8,10 @@ import time
 app = Flask(__name__)
 
 # Configure Serial (change based on your system)
-<<<<<<< HEAD
-SERIAL_PORT = "/dev/cu.usbmodem157920201"  # On Windows: "COM3", Linux/Mac: "/dev/ttyUSB0" or "/dev/cu.usbmodem..."
-BAUD_RATE = 9600
-=======
+
 SERIAL_PORT = "/dev/tty.usbserial-D30873OK"  # On Windows: "COM3", Linux/Mac: "/dev/ttyUSB0" or "/dev/cu.usbmodem..."
 BAUD_RATE = 115200
->>>>>>> e6f94d6a053c623990c89a05f1ca459090cdfd1f
+
 
 # Connect to Arduino
 ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=3)
@@ -32,16 +29,8 @@ def read_sensor_data():
     global sensor_data
     while True:
         try:
-<<<<<<< HEAD
-            if 1:#ser.in_waiting > 0:
+            if ser.in_waiting > 0: 
                 line = ser.readline().decode("utf-8").strip()
-                print(line)
-=======
-            if ser.in_waiting > 0:
-                
-                line = ser.readline().decode("utf-8").strip()
-               
->>>>>>> e6f94d6a053c623990c89a05f1ca459090cdfd1f
                 if line:  # Ensure data is not empty
                     new_data = json.loads(line)
                     print(new_data)
@@ -58,23 +47,8 @@ def read_sensor_data():
                     if len(sensor_history["timestamps"]) > MAX_HISTORY:
                         for key in sensor_history:
                             sensor_history[key].pop(0)
-<<<<<<< HEAD
-            else:
-                print("happy")
-            time.sleep(.5)
-        except serial.SerialException:
-            print("Serial device disconnected. Retrying...")
-            reconnect_serial()
-        except json.JSONDecodeError:
-            print("bad data")
-=======
-          
-        # except serial.SerialException:
-        #     print("Serial device disconnected. Retrying...")
-        #     reconnect_serial()
         except json.JSONDecodeError:
             print("json wrong")
->>>>>>> e6f94d6a053c623990c89a05f1ca459090cdfd1f
             # Ignore malformed data
             pass
 
